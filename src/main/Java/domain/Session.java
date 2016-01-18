97d25429aaff11842d0f062991be3729a70db5ed
eiriksandberg <eirik.sandberg@live.no>
2016-01-18 12:20:28 +0100

    Added domain

--- src/main/Java/domain/Session.java
+++ src/main/Java/domain/Session.java
@@ -1,7 +1,72 @@
 package domain;
 
+import java.util.Date;
+
 /**
  * Created by eiriksandberg on 18.01.2016.
  */
 public class Session {
+    private String title;
+    private String description;
+    private Date date;
+    private Date startTime;
+    private Date endTime;
+    private String location;
+
+    public Session(String title, String description, Date date, Date startTime, Date endTime, String location) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
