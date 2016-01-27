84ace603c7590600fc74c32065b2fb27cec6394c
eiriksandberg <eirik.sandberg@live.no>
2016-01-27 09:23:47 +0100

    Added service to angular

--- src/main/webapp/resources/js/sessionRegisterApp.js
+++ src/main/webapp/resources/js/sessionRegisterApp.js
@@ -4,9 +4,33 @@
 var myApp = angular.module('registerApp', ['ngAnimate', 'mgcrea.ngStrap', 'ngRoute']);
 
 myApp.controller('AddSessionCtrl', function($scope, $modal){
-    var myModal = $modal({scope: $scope, template: '/resources/html/registerSessionModal.html', show: false});
+    var myModal = $modal({scope: $scope, templateUrl: '/resources/html/registerSessionModal.html', show: false});
     $scope.showModal = function() {
         myModal.$promise.then(myModal.show);
     };
+    $scope.hideModal = function() {
+        myModal.$promise.then(myModal.show(false));
+    };
 });
 
+
+myApp.controller('AddInfoCtrl', ['$scope', 'sessionService', function($scope, sessionService){
+    $scope.session = sessionService.get();
+    $scope.update = function(newSession){
+        sessionService.add(newSession);
+    }
+}]);
+
+myApp.factory('sessionService', function() {
+    var session = {};
+    var sessionService = {};
+
+    sessionService.add = function(newSession) {
+        session = angular.copy(newSession)
+    };
+    sessionService.get = function() {
+        return session;
+    };
+
+    return sessionService;
+});
