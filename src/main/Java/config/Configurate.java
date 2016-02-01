6cef3c8f08c2b7fd177382dc78720660c0535aa7
Lars <garbergla@gmail.com>
2016-02-01 16:00:19 +0100

    Database-mock objects can now be used.

--- src/main/Java/config/Configurate.java
+++ src/main/Java/config/Configurate.java
@@ -2,10 +2,15 @@
 package config;
 
 import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.*;
 import org.springframework.web.servlet.*;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.handler.*;
 import org.springframework.web.servlet.view.*;
+import repository.*;
+import service.*;
+
+import java.sql.*;
 
 @Configuration
 @EnableWebMvc  // mvc annotation
@@ -33,5 +38,38 @@ public class Configurate extends WebMvcConfigurationSupport {
         AbstractHandlerMapping handlerMapping = (AbstractHandlerMapping) super.resourceHandlerMapping();
         handlerMapping.setOrder(-1);
         return handlerMapping;
+    }
+
+    // NYTT etter Database-impl..
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource(){
+        String url = "jdbc:derby://localhost:1527/testDB;create=true";
+        DriverManagerDataSource dmds = new DriverManagerDataSource(url);
+        dmds.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
+        try{
+            Connection con = dmds.getConnection();
+            System.out.println(" *********  Konfig " + con );
+            //getAllePersoner(con); //brukes for testing av oppkobling
+        }catch(Exception e){
+            System.out.println(" Konfig.Feil ved henting av conncetion() " + e);
+        }
+        return dmds;
+    }
+
+    @Bean
+    public PersonRepository repository(){
+        return new PersonDatabaseJdbcTemplateRepositoryImpl();
+        //return new PersonDatabaseRepositoryImpl();
+    }
+
+    @Bean
+    public PersonService personService(){
+        return new PersonServiceImpl();
     }
 }
--- src/main/Java/controller/homeController.java
+++ src/main/Java/controller/homeController.java
@@ -1,20 +1,30 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import service.*;
+import ui.*;
 
 /**
  * Created by Lars on 13.01.16.
  */
 @Controller
 public class homeController {
+
+    @Autowired
+    private PersonService personService;
+
     @RequestMapping("/")
     public String home(){
         return "registerCourse";
     }
 
     @RequestMapping("/event")
-    public String event(){ return "event";}
+    //public String event(){ return "event";}
+    public String event(@ModelAttribute PersonFormBackingBean backingBean) {
+        backingBean.setAllePersoner(personService.getAllePersoner());
+        System.out.println("** ControllerClass.person() ******");
+        return "event";
+    }
 }
